3b52e1890f012ff085048f583be63edbe2954cd9
Bima42 <tanpau42@gmail.com>
2021-12-07 15:37:11 +0100

    reception and unicode works

--- client.c
+++ client.c
@@ -8,19 +8,32 @@ void	send_char_by_bit(unsigned char c, int pid)
 	while (++i < 8)
 	{
 		if (c & 0x01)
+		{
 			kill(pid, SIGUSR2);
+			pause();
+		}
 		else
+		{
 			kill(pid, SIGUSR1);
+			pause();
+		}
 		c = c >> 1;
 		usleep(100);
 	}
 }
 
+void	handler_client(int signal)
+{
+	(void)signal;
+	write(1, "ok", 2);
+}
+
 int	main(int argc, char **argv)
 {
-	int		pid;
-	int		i;
+	int	pid;
+	int	i;
 	char	*str;
+	struct sigaction	sa;
 
 	if (argc != 3)
 	{
@@ -30,6 +43,10 @@ int	main(int argc, char **argv)
 	i = 0;
 	pid = ft_atoi(argv[1]);
 	str = argv[2];
+	sa.sa_handler = &handler_client;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (str[i])
 		send_char_by_bit(str[i++], pid);
 }
